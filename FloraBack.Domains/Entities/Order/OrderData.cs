d9f4e3a2cd115ec44f40d42aa4999dd4a283c554
MayEnjoyer <lemurkanikitka@mail.ru>
2026-04-21 11:46:25 +0300

    implemented Order entity + OrderItem

---
+++ FloraBack.Domains/Entities/Order/OrderData.cs
@@ -0,0 +1,21 @@
+﻿using FloraBack.Domains.Entities.Refs;
+using FloraBack.Domains.Entities.Address;
+using FloraBack.Domains.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraBack.Domains.Entities.Order
+{
+    public class OrderData : SharedFields
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public List<OrderItem> Items { get; set; }
+        public decimal TotalPrice { get; set; }
+        public AddressData DeliveryAddress { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
---
+++ FloraBack.Domains/Enums/OrderStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraBack.Domains.Enums
+{
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Canceled = 2,
+        Shipping = 3,
+        Delievered = 4,
+    }
+}
