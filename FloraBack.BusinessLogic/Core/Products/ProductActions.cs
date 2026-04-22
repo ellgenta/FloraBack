1f65f149545b9b06017e42d4546447d0b8587f43
Persik <lia.griscenco@isa.utm.md>
2026-04-22 19:22:22 +0300

    Refactored product CRUD for in-memory repository

--- FloraBack.BusinessLogic/Core/Products/ProductActions.cs
+++ FloraBack.BusinessLogic/Core/Products/ProductActions.cs
@@ -1,4 +1,5 @@
-﻿using FloraBack.Domains.Entities.Product;
+﻿using FloraBack.BusinessLogic.Interface;
+using FloraBack.Domains.Entities.Product;
 using FloraBack.Domains.Enums;
 using FloraBack.Domains.Models.Product;
 
@@ -6,221 +7,68 @@ namespace FloraBack.BusinessLogic.Core.Products
 {
     public class ProductAction
     {
-        protected List<ProductData> ExecuteGetAllProductsAction()
+        static List<ProductData> _DataRepo = new List<ProductData>();
+        static int _nextId = 1;
+        public List<ProductData> ExecuteGetAllProductsAction()
         {
-            var products = new List<ProductData>();
-
-            var productFromDb = new ProductData()
-            {
-                Id = 1,
-                Name = "Product 1",
-                Description = new ProductDescriptionData()
-                {
-                    Id = 1,
-                    Description = "Description 1",
-                    DescriptionAdvanced = new DescriptionAdvanced()
-                    {
-                        Id = 1,
-                        H = 20,
-                        W = 10,
-                        L = 5
-                    }
-                },
-                Category = new CategoryData()
-                {
-                    Id = 1,
-                    Name = ProductCategory.Plants
-                },
-                Images = new List<ProductImgData>()
-                {
-                    new ProductImgData()
-                    {
-                        Id = 1,
-                        Url = "https://example.com/product1.jpg",
-                        ProductId = 1
-                    },
-                    new ProductImgData()
-                    {
-                        Id = 2,
-                        Url = "https://example.com/product1-2.jpg",
-                        ProductId = 1
-                    }
-                },
-                Price = 100.20m,
-                Status = ProductStatus.Active,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            products.Add(productFromDb);
-            return products;
+            return _DataRepo;
         }
 
-        protected ProductData? ExecuteGetProductByIdAction(int id)
+        public ProductData? ExecuteGetProductByIdAction(int id)
         {
-            var productFromDb = new ProductData()
+            foreach (var _product in _DataRepo)
             {
-                Id = 1,
-                Name = "Product 1",
-                Description = new ProductDescriptionData()
+                if (_product.Id == id)
                 {
-                    Id = 1,
-                    Description = "Description 1",
-                    DescriptionAdvanced = new DescriptionAdvanced()
-                    {
-                        Id = 1,
-                        H = 20,
-                        W = 10,
-                        L = 5
-                    }
-                },
-                Category = new CategoryData()
-                {
-                    Id = 1,
-                    Name = ProductCategory.Plants
-                },
-                Images = new List<ProductImgData>()
-        {
-            new ProductImgData()
-            {
-                Id = 1,
-                Url = "https://example.com/product1.jpg",
-                ProductId = 1
-            }
-        },
-                Price = 100.20m,
-                Status = ProductStatus.Active,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            if (productFromDb.Id != id)
-            {
-                return null;
+                    return _product;
+                }
             }
 
-            return productFromDb;
+            return null;
         }
 
-        protected List<ProductData> ExecuteGetProductsByCategoryAction(ProductCategory category)
+        public List<ProductData> ExecuteGetProductsByCategoryAction(ProductCategory category)
         {
-            var products = new List<ProductData>();
+            var _products = new List<ProductData>();
 
-            var productFromDb = new ProductData()
+            foreach (var _product in _DataRepo)
             {
-                Id = 1,
-                Name = "Product 1",
-                Description = new ProductDescriptionData()
+                if (_product.Category != null && _product.Category.Name == category)
                 {
-                    Id = 1,
-                    Description = "Description 1",
-                    DescriptionAdvanced = new DescriptionAdvanced()
-                    {
-                        Id = 1,
-                        H = 20,
-                        W = 10,
-                        L = 5
-                    }
-                },
-                Category = new CategoryData()
-                {
-                    Id = 1,
-                    Name = ProductCategory.Plants
-                },
-                Images = new List<ProductImgData>()
-        {
-            new ProductImgData()
-            {
-                Id = 1,
-                Url = "https://example.com/product1.jpg",
-                ProductId = 1
-            }
-        },
-                Price = 100.20m,
-                Status = ProductStatus.Active,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            if (productFromDb.Category.Name == category)
-            {
-                products.Add(productFromDb);
+                    _products.Add(_product);
+                }
             }
 
-            return products;
+            return _products;
         }
 
-        protected List<ProductData> ExecuteGetProductsBySubCategoryAction(string subCategory)
+        public List<ProductData> ExecuteGetProductsBySubCategoryAction(string subCategory)
         {
-            var products = new List<ProductData>();
+            var _products = new List<ProductData>();
 
-            var productFromDb = new ProductData()
-            {
-                Id = 1,
-                Name = "Product 1",
-                Description = new ProductDescriptionData()
+            foreach (var _product in _DataRepo) {
+                if (_product.Category != null &&
+                    _product.Category.SubCategories != null)
                 {
-                    Id = 1,
-                    Description = "Description 1",
-                    DescriptionAdvanced = new DescriptionAdvanced()
+                    foreach (var _subCategory in _product.Category.SubCategories)
                     {
-                        Id = 1,
-                        H = 20,
-                        W = 10,
-                        L = 5
+                        if (_subCategory.Equals(subCategory, StringComparison.OrdinalIgnoreCase))
+                        {
+                            _products.Add(_product);
+                            break;
+                        }
                     }
-                },
-                Category = new CategoryData()
-                {
-                    Id = 1,
-                    Name = ProductCategory.Plants,
-                    SubCategories = new List<string>()
-            {
-                "Indoor",
-                "Large"
+                }
             }
-                },
-                Images = new List<ProductImgData>()
-        {
-            new ProductImgData()
-            {
-                Id = 1,
-                Url = "https://example.com/product1.jpg",
-                ProductId = 1
-            },
-            new ProductImgData()
-            {
-                Id = 2,
-                Url = "https://example.com/product1-2.jpg",
-                ProductId = 1
-            }
-        },
-                Price = 100.20m,
-                Status = ProductStatus.Active,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            if (productFromDb.Category.SubCategories != null &&
-                productFromDb.Category.SubCategories.Any(x => x.Equals(subCategory, StringComparison.OrdinalIgnoreCase)))
-            {
-                products.Add(productFromDb);
-            }
-
-            return products;
+            return _products;
         }
 
 
-        protected ProductData? ExecuteCreateProductAction(ProductData product)
+        public ProductData? ExecuteCreateProductAction(ProductData product)
         {
-            if (product == null)
+            var _newProduct = new ProductData()
             {
-                return null;
-            }
-
-            var newProduct = new ProductData()
-            {
-                Id = product.Id > 0 ? product.Id : 2,
+                Id = _nextId++,
                 Name = product.Name,
                 Description = product.Description,
                 Category = product.Category,
@@ -230,76 +78,41 @@ namespace FloraBack.BusinessLogic.Core.Products
                 CreatedAt = DateTime.Now,
                 UpdatedAt = DateTime.Now
             };
-
-            return newProduct;
+            _DataRepo.Add(_newProduct);
+            return _newProduct;
         }
 
-        protected ProductData? ExecuteUpdateProductAction(int id, ProductData product)
+        public ProductData? ExecuteUpdateProductAction(int id, ProductData product)
         {
-            var productFromDb = new ProductData()
+            foreach (var _product in _DataRepo)
             {
-                Id = 1,
-                Name = "Product 1",
-                Description = new ProductDescriptionData()
+                if (_product.Id == id)
                 {
-                    Id = 1,
-                    Description = "Description 1",
-                    DescriptionAdvanced = new DescriptionAdvanced()
-                    {
-                        Id = 1,
-                        H = 20,
-                        W = 10,
-                        L = 5
-                    }
-                },
-                Category = new CategoryData()
-                {
-                    Id = 1,
-                    Name = ProductCategory.Plants
-                },
-                Images = new List<ProductImgData>()
-        {
-            new ProductImgData()
-            {
-                Id = 1,
-                Url = "https://example.com/product1.jpg",
-                ProductId = 1
+                    _product.Name = product.Name;
+                    _product.Description = product.Description;
+                    _product.Category = product.Category;
+                    _product.Images = product.Images;
+                    _product.Price = product.Price;
+                    _product.Status = product.Status;
+                    _product.UpdatedAt = DateTime.Now;
+
+                    return _product;
+                }
             }
-        },
-                Price = 100.20m,
-                Status = ProductStatus.Active,
-                CreatedAt = DateTime.Now,
-                UpdatedAt = DateTime.Now
-            };
-
-            if (productFromDb.Id != id)
-            {
-                return null;
-            }
-
-            productFromDb.Name = product.Name;
-            productFromDb.Description = product.Description;
-            productFromDb.Category = product.Category;
-            productFromDb.Images = product.Images;
-            productFromDb.Price = product.Price;
-            productFromDb.UpdatedAt = DateTime.Now;
-
-            return productFromDb;
+            return null;
         }
 
-        protected bool ExecuteDeleteProductAction(int id)
+        public bool ExecuteDeleteProductAction(int id)
         {
-            var productFromDb = new ProductData()
+            foreach (var _product in _DataRepo)
             {
-                Id = 1
-            };
-
-            if (productFromDb.Id != id)
-            {
-                return false;
+                if (_product.Id == id)
+                {
+                    _DataRepo.Remove(_product);
+                    return true;
+                }
             }
-
-            return true;
+            return false;
         }
     }
 }
