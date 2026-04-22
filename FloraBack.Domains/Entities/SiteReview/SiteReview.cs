dadf3863c9a8d7771fb02e2b6eef2d83de4fa062
MayEnjoyer <lemurkanikitka@mail.ru>
2026-04-22 21:03:24 +0300

    created SiteReview entity

---
+++ FloraBack.Domains/Entities/SiteReview/SiteReview.cs
@@ -0,0 +1,18 @@
+﻿using FloraBack.Domains.Entities.Refs;
+using FloraBack.Domains.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraBack.Domains.Entities.SiteReview
+{
+    public class SiteReview : SharedFields
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public ReviewNote Note { get; set; }
+    }
+}
---
+++ FloraBack.Domains/Enums/ReviewNote.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraBack.Domains.Enums
+{
+    public enum ReviewNote
+    {
+        Poor = 1,
+        Bad = 2,
+        Ok = 3,
+        Good = 4,
+        Amazing = 5,
+    }
+}
