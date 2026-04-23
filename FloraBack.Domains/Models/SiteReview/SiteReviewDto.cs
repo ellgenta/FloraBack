fcb9aa70b62790ce96d1efd6ebc2fee76eee363d
TreePlanter <lemurkanikitka@mail.ru>
2026-04-23 17:11:35 +0300

    added items for future CRUD implementation (for SiteReview)

---
+++ FloraBack.Domains/Models/SiteReview/SiteReviewDto.cs
@@ -0,0 +1,17 @@
+﻿using FloraBack.Domains.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraBack.Domains.Models.SiteReview
+{
+    public class SiteReviewDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public ReviewNote Note { get; set; }
+    }
+}
---
+++ FloraBack.api/Controller/SiteReviewController.cs
@@ -0,0 +1,13 @@
+﻿using FloraBack.BusinessLogic.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FloraBack.Api.Controller
+{
+    [Route("api/review/site")]
+    [ApiController]
+
+    public class SiteReviewController : ControllerBase
+    {
+        private ISiteReviewActions _siteReviewActions;
+    }
+}
