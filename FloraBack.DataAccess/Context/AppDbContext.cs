388465630c9185fde4ef34e46faefa44458e7237
MayEnjoyer <lemurkanikitka@mail.ru>
2026-05-13 22:30:48 +0300

    got rid of divided contexts, started rewriting the structure

---
+++ FloraBack.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using FloraBack.Domains.Entities.Order;
+using FloraBack.Domains.Entities.User;
+using FloraBack.Domains.Entities.SiteReview;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraBack.DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<UserData> Users { get; set; }
+
+        public DbSet<SiteReviewData> SiteReviews { get; set; }
+
+        public DbSet<OrderData> Orders { get; set; }
+
+        public DbSet<OrderItemData> OrderItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbSession.ConnectionStrings);
+        }
+    }
+}
