88034f3207ba5be4a34f38fc72cda27e6efcda02
Persik <lia.griscenco@isa.utm.md>
2026-05-16 22:49:23 +0300

    Add category and subcategory EF configurations

---
+++ FloraBack.DataAccess/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using FloraBack.Domains.Entities.Category;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FloraBack.DataAccess.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<CategoryData>
+    {
+        public void Configure(EntityTypeBuilder<CategoryData> builder)
+        {
+            builder.ToTable("Categories");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasMany(c => c.SubCategories)
+                .WithOne(sc => sc.Category)
+                .HasForeignKey(sc => sc.CategoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasMany(c => c.Products)
+                .WithOne(p => p.Category)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ FloraBack.DataAccess/Configurations/SubCategoryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using FloraBack.Domains.Entities.Category;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FloraBack.DataAccess.Configurations
+{
+    public class SubCategoryConfiguration : IEntityTypeConfiguration<SubCategoryData>
+    {
+        public void Configure(EntityTypeBuilder<SubCategoryData> builder)
+        {
+            builder.ToTable("SubCategories");
+
+            builder.HasKey(sc => sc.Id);
+
+            builder.Property(sc => sc.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasOne(sc => sc.Category)
+                .WithMany(c => c.SubCategories)
+                .HasForeignKey(sc => sc.CategoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasMany(sc => sc.Products)
+                .WithOne(p => p.SubCategory)
+                .HasForeignKey(p => p.SubCategoryId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
